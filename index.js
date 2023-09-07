a0291816885aac8f77afa2571406c66a9bc97ec7
agwadan <agwa.dan@gmail.com>
2023-09-07 14:54:31 +0300

    Add functionality to add task

---
+++ index.js
@@ -0,0 +1,19 @@
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const addTaskButton = document.getElementById('addTask');
+const showAllButton = document.getElementById('showAll');
+const showCompletedButton = document.getElementById('showCompleted');
+const showUnCompletedButton = document.getElementById('unshowCompleted');
+
+addTaskButton.addEventListener('click', () => {
+  if (taskInput.value.trim() === '') return;
+
+  console.log('task added');
+  const li = document.createElement('li');
+  li.addEventListener('click',() => {
+    li.classList.toggle('completed');
+  })
+  li.textContent = taskInput.value;
+  taskList.appendChild(li);
+  taskInput.value = '';
+})
